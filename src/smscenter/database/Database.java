592b11b3fb05793b87a99504f1a4037abc4030a0
christoph <christoph@7754d8ab-9b54-e011-a251-0001c006bdc7>
2025-06-08 11:45:06 +0000

    Fix SQL error
    git-svn-id: https://localhost/svn/SMSCenter/trunk@9347 7754d8ab-9b54-e011-a251-0001c006bdc7

--- src/smscenter/database/Database.java
+++ src/smscenter/database/Database.java
@@ -1269,7 +1269,7 @@ public class Database {
         List<Match> list = new java.util.ArrayList<>();
         Connection conn;
         String sql = 
-                "SELECT cp.cpID, cpName, cpDesc, cpType, " +
+                "SELECT gr.cpID, cp.cpName, cpDesc, cpType, " +
                 "       gr.grID, grName, grDesc, grStage, grModus, " +
                 "       mt.mtNr, mtBestOf, mtTimeStamp, " +
                 "       mtTable, mtDateTime, mtResA, mtResX, " +
@@ -1291,7 +1291,7 @@ public class Database {
                 "       LEFT OUTER JOIN smscenter_phones phoneX ON mt.plXplNr = phoneX.plNr " +
                 " WHERE cp.cpType = 1 AND scr.ts < DATEADD(second, " + (-delay) + ", CURRENT_TIMESTAMP) " +
                 "UNION " +
-                "SELECT cp.cpID, cpName, cpDesc, cpType, " +                
+                "SELECT gr.cpID, vp.cpName, cpDesc, cpType, " +                
                 "       gr.grID, grName, grDesc, grStage, grModus, " +
                 "       mt.mtNr, mtBestOf, mtTimeStamp, " +
                 "       mtTable, mtDateTime, mtResA, mtResX, " +
